6f7df51443490868ce0ff51ed4381157c6a4e327
sujnyankini <156564644+sujnyankini@users.noreply.github.com>
2024-01-30 10:57:10 +0530

    Add files via upload

---
+++ Lab-7/Mainexception.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner
+class WrongAge extends Exception {
+
+public WrongAge(){
+	super("Error message");
+}
+
+
+   public  WrongAge(String message) {
+        super(message);
+    }
+
+}
+
+class inputScanner {
+    static Scanner sc = new Scanner(System.in);
+}
+
+class Father extends inputScanner {
+    int fatherage;
+
+    Father() throws WrongAge {
+        System.out.println("Enter Father's age");
+        fatherAge = sc.nextInt();
+        if (fatherAge < 0) {
+            throw new WrongAge("Age cannot be negative");
+
+        }
+    }
+
+   
+ void display() {
+        System.out.println("Father's age is " + fatherAge);
+    }
+
+}
+
+class Son extends Father {
+    int sonage;
+
+    Son() throws WrongAge {
+        System.out.println("Enter son's age");
+        sonage = sc.nextInt();
+        if (sonAge > fatherAge) {
+            throw new WrongAge("Son's age cannot be greater than father's age");
+        } 
+	else if (sonAge < 0) {
+            throw new WrongAge("Age cannot be negative");
+        }
+
+    }
+
+    void display() {
+        System.out.println("Son's age is " + sonage);
+    }
+
+}
+
+public class Mainexception {
+    public static void Main(String args[]) {
+        try{
+            Son son= new Son();
+            son.display();
+        }
+        catch(WrongAge e){
+            System.out.println("Exception "+ e.getMessage());
+        }
+
+
+    }
+}
